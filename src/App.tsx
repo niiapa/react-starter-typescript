5d77281e878d1c8dd04bb246c4e4562b1fcb3200
Nii Apa Abbey <abbeyniiapa@gmail.com>
2019-06-24 03:54:22 +0200

    Fixed export

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import {Component} from "react";
 
-export class App extends Component {
+export default class App extends Component {
 	render() {
 		return (
-			<div>
+			<div data-test-id='app-root'>
 				<h1>
 					Typescript Web App here!
 				</h1>
