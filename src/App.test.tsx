f28b128bc93fe0f8a22995c7aebe5e525f17d329
Nii Apa Abbey <abbeyniiapa@gmail.com>
2019-06-24 03:54:15 +0200

    Create App.test.tsx

---
+++ src/App.test.tsx
@@ -0,0 +1,11 @@
+import {shallow} from "enzyme";
+import App from "./App";
+import * as React from "react";
+
+describe("<App />", () => {
+	it("renders without crashing", () => {
+		const wrapper = shallow(<App />);
+
+		expect(wrapper.exists('div')).toBeTruthy();
+	});
+});
