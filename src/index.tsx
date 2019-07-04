1bf0c31b394398cc1725a09b5a1f6dcf76f31a19
Nii Apa Abbey <abbeyniiapa@gmail.com>
2019-07-04 02:19:09 +0100

    Renamed style.scss to styles.scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import './scss/style.scss';
+import './scss/styles.scss';
 import App from './App';
 
 render(<App />, document.getElementById('root'));
